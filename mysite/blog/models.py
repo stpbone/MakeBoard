f4cc0af4819d853b43184280c4c2483bb0debdd4
Simon <simonhthomasbone@gmail.com>
2018-12-29 18:20:12 +0000

    updated models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    target_customer = models.TextField()
 
 
 
